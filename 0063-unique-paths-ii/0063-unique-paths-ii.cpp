fe6ce9aa9adc777fce31c91005139a756670efbb
Shivani-1560 <shivaniagarwal1215@gmail.com>
2026-01-08 23:19:25 +0530

    Time: 0 ms (100%), Space: 12 MB (5.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
 
     int not_takeObs(vector<vector<int>>& obstacleGrid,int i,int j,vector<vector<int>> &dp){
-          if(obstacleGrid[0][0]==1 ) return 0;
+        if(i<0 || j<0) return 0;
+          if(obstacleGrid[i][j]==1 ) return 0;
        if(i==0 && j==0) return 1;
-       if(i<0 || j<0) return 0;
+       
        if(dp[i][j]!= -1) return dp[i][j];
        int up=not_takeObs(obstacleGrid,i-1,j,dp);
        int left=not_takeObs(obstacleGrid,i,j-1,dp);
-        if(obstacleGrid[i][j]== 0){
-       return dp[i][j]=up+left;
-        }
-       return dp[i][j]=0;
-    }
+         return dp[i][j]=up+left;
+      }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
