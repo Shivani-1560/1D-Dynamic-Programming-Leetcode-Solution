e959e4ea541c7cf31de58ad9810c3d5c8628149b
Shivani-1560 <shivaniagarwal1215@gmail.com>
2026-01-10 16:15:43 +0530

    Time: 3 ms (8.56%), Space: 8.9 MB (6.07%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+       vector<int>dp(n+1,false);
+
+       for(int i=2;i<=n;i++){
+        for(int x=1;x<i;x++){
+
+            if(i%x ==0 && dp[i-x]==false){
+                dp[i]=true;
+                break;
+            }
+        }
+       }
+       return dp[n];
+    }
+};
